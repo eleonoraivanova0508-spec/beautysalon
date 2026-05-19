320ac13a075ce739927d9684a4ae0bc7e906f949
andrey <eleonoraivanova0508@gmail.com>
2026-05-19 18:45:01 +0500

    fix render deployment django

--- salon/settings.py
+++ salon/settings.py
@@ -1,12 +1,12 @@
 from pathlib import Path
-
+import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-secret-key'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Приложения
 INSTALLED_APPS = [
@@ -30,6 +30,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'salon.urls'
@@ -86,11 +88,11 @@ USE_TZ = True
 
 # Статика
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Тип ID
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
